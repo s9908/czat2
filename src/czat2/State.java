f86d5a650ce6b6a08d24dad3ca54a2e064dbe527
s9908 <s9908@pjwstk.edu.pl>
2014-06-08 13:38:28 +0200

    podstawowy czat

--- src/czat2/State.java
+++ src/czat2/State.java
@@ -8,21 +8,47 @@ import javax.faces.bean.ManagedBean;
 public class State 
 {
 	String txt_tresc;
+	String editor;
+	
+	public void wyslij()
+	{
+		if(editor.trim().length() > 0)
+		{
+			editor = editor.replaceAll(":\\)", "<img src='./img/usmiech.png' width='16'>");
+			editor = editor.replaceAll(":\\*", "<img src='./img/kiss.png' width='16'>");
+			String txt = "<font color='brown'>";
+			txt += "XXXX";
+			txt += "</font>" + editor;
+			txt_tresc += txt + "<br/>";
+			editor = "";
+		}
+	}
 
 	public State()
 	{
 		txt_tresc = "SYSTEM: Uruchomienie czatu.<br/>";
 	}
-
+	
+	
 	public String getTxt_tresc() 
 	{
 		return txt_tresc;
 	}
-
 	public void setTxt_tresc(String txt_tresc) 
 	{
 		this.txt_tresc = txt_tresc;
 	}
+
+	public String getEditor() 
+	{
+		return editor;
+	}
+
+	public void setEditor(String editor) 
+	{
+		this.editor = editor;
+	}
+	
 	
 	
     
